02cb232a5ef70f494cc42496d64943151fe05231
Sinan Şar <32620910+sinansar123@users.noreply.github.com>
2021-01-19 21:55:04 +0100

    Add files via upload

---
+++ amps.py
@@ -0,0 +1,15 @@
+
+
+
+class Amp():
+    def __init__(self, model, brand, prod_year, watts, tubes, mic, link, added_by, id=None):
+        self.id = id
+        self.model = model
+        self.brand = brand
+        self.prod_year = prod_year
+        self.watts = watts
+        self.tubes = tubes
+        self.mic = mic
+        self.link = link
+        self.added_by = added_by
+
